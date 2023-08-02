79a715eb33174ea3772dee10cc13e29918606929
Jaakko Heusala <jhh@hg.fi>
2023-08-02 23:02:21 +0300

    Added disposable interface

--- EmailVerificationService.ts
+++ EmailVerificationService.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2021-2022. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
+// Copyright (c) 2021-2023. Heusala Group Oy <info@heusalagroup.fi>. All rights reserved.
 
 import { randomInt } from 'crypto';
 import { find } from "../core/functions/find";
@@ -6,6 +6,7 @@ import { forEach } from "../core/functions/forEach";
 import { remove } from "../core/functions/remove";
 import { LogService } from "../core/LogService";
 import { clearTimeout } from "timers";
+import { Disposable } from "../core/types/Disposable";
 import { LogLevel } from "../core/types/LogLevel";
 
 const DEFAULT_VERIFICATION_TIMEOUT : number = 5*60*1000;
@@ -18,7 +19,7 @@ interface InternalEmailCode {
 
 const LOG = LogService.createLogger('EmailVerificationService');
 
-export class EmailVerificationService {
+export class EmailVerificationService implements Disposable {
 
     public static setLogLevel (level: LogLevel) {
         LOG.setLogLevel(level);
